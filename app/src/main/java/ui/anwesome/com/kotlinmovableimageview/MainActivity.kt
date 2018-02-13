330ca0bfdc04196701d11fc024647a0cf250cd94
Anwesh43 <anweshthecool0@gmail.com>
2018-02-13 08:13:06 +0530

    creating MovableImageView create method

--- app/src/main/java/ui/anwesome/com/kotlinmovableimageview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinmovableimageview/MainActivity.kt
@@ -1,12 +1,14 @@
 package ui.anwesome.com.kotlinmovableimageview
 
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.movableimageview.MovableImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MovableImageView.create(this, BitmapFactory.decodeResource(resources, R.drawable.nature_more))
     }
 }
