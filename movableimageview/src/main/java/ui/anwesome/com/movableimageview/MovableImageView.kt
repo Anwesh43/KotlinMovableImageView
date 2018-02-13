f55825c3c2a9231b31e170a63a3f570250c8e111
Anwesh43 <anweshthecool0@gmail.com>
2018-02-13 07:43:19 +0530

    adding movableimageview

--- movableimageview/src/main/java/ui/anwesome/com/movableimageview/MovableImageView.kt
+++ movableimageview/src/main/java/ui/anwesome/com/movableimageview/MovableImageView.kt
@@ -5,4 +5,18 @@ package ui.anwesome.com.movableimageview
  */
 import android.content.*
 import android.graphics.*
-import android.content.*
+import android.view.*
+class MovableImageView(ctx:Context,var bitmap:Bitmap):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
